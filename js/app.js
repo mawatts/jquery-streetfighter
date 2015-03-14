0e69a949bf845ef89f445ba9082c88e1ac187025
mawatts <michaelwatts80@yahoo.com>
2015-03-14 13:57:52 -0400

    Add js to animate hadouken throw

--- js/app.js
+++ js/app.js
@@ -1,11 +1,24 @@
 $(document).ready(function() {
-	/*Move Ryu into Ready position*/
 	$('.ryu').mouseenter(function() {
+		//Show Ryu ready position gif and hide still image
 		$('.ryu-still').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function() {
+		//Show Ryu still image and hide ready position gif
 		$('.ryu-still').show();
 		$('.ryu-ready').hide();
+	})
+	.mousedown(function() {
+		//Play hadouken sound, hide ready gif, and show throwing gif
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').show();
+		// animate hadouken to the right of the screen
+	})
+	.mouseup(function() {
+		//Hide throwing gif and show ready gif
+		$('.ryu-throwing').hide();
+		$('.ryu-ready').show();
 	});
 });
