436c1b1eb67cb36d6836d354ad88c5931fdafabb
mawatts <michaelwatts80@yahoo.com>
2015-03-14 13:44:10 -0400

    Add JS Ryu ready function to show/hide images/gifs

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	/*Move Ryu into Ready position*/
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+	.mouseleave(function() {
+		$('.ryu-still').show();
+		$('.ryu-ready').hide();
+	});
+});
